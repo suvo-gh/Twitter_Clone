0642d93de3e375a193436bc2e5496df1a4782d2f
suvo-gh <suvo@techis.io>
2021-11-29 11:18:46 +0530

    requirements

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -123,12 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-
-STATICFILES_DIRS = [BASE_DIR / 'static',
-            '/var/www/static',]
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 cloudinary.config(
     cloud_name="www-techis-io",
