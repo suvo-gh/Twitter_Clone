7a2eb7b9ee7cc7bc1febebd53163911392ecccd7
SuvoPC <suvo@techis.io>
2021-11-29 20:33:41 +0530

    testing static

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -123,12 +123,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# STATIC_URL = '/static/'
+
+# STATIC_ROOT = "/var/www/static"
+
+# STATICFILES_DIRS = [BASE_DIR / 'static',
+#             ]
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = "/var/www/static"
-
-STATICFILES_DIRS = [BASE_DIR / 'static',
-            ]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 cloudinary.config(
